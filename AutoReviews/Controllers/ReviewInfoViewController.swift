3315d706482a60c2e1f5e24156157dc5d4cc4f0f
Svetlana <svetaromanenko@inbox.ru>
2021-02-23 19:16:28 +0300

    Created ListOfReviewsTableViewController and ReviewInfoViewController, ListOfReviewsStoryboard

---
+++ AutoReviews/Controllers/ReviewInfoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ReviewInfoViewController.swift
+//  AutoReviews
+//
+//  Created by Светлана Романенко on 23.02.2021.
+//
+
+import UIKit
+
+class ReviewInfoViewController: UIViewController {
+    
+    @IBOutlet var modificationLabel: UILabel!
+    @IBOutlet var tittleLabel: UILabel!
+    @IBOutlet var contentLabel: UILabel!
+    
+    var review: Review!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        modificationLabel.text = review.modification
+        tittleLabel.text = review.title
+        contentLabel.text = review.content
+    }
+    
+    @IBAction func backToReviewsButtonPressed() {
+        dismiss(animated: true)
+    }
+}
